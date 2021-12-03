667803de7fb65e0b60d86a6cb3aca891e06d07dc
kylo74 <kyloc74@gmail.com>
2021-12-03 15:13:36 +0000

    Set up structure for game funcitons

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementwWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractionnQuestion(){
+
+}
+
+function displayMultiplynQuestion(){
+
+}
+
+function displayAdditionQuestion(){
+    
+}
